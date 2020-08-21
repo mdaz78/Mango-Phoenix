6bedbc63b992c2d7a6e835f3000870b2bf451928
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-08-21 11:20:38 +0530

    Update port number on dev from 5000 to 4000

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :mango, Mango.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :mango, MangoWeb.Endpoint,
-  http: [port: 5000],
+  http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
